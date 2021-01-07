c991f5b26b6306991b4c6c28180aa0d112d1cd47
HyeonGiMin <hyeoni1995@gmail.com>
2021-01-07 22:05:13 +0900

    ch09-6 finish

---
+++ 09/ch09-6/src/model/coordinates/index.ts
@@ -0,0 +1,5 @@
+import {ICoordinates} from './ICoordinates'
+import {makeICoordinates} from './makeICoordinates'
+import {makeRandomICoordinates} from './makeRandomIcoordinates'
+
+export {ICoordinates,makeICoordinates,makeRandomICoordinates}
