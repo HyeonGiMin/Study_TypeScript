89137c6e175390975a1dd3f4872cabaef9ecdc43
HyeonGiMin <hyeoni1995@gmail.com>
2021-01-29 22:48:03 +0900

    20210129

--- 12/ch12-2/src/mongodb/connect.ts
+++ 12/ch12-2/src/mongodb/connect.ts
@@ -1,4 +1,4 @@
 import MongoClient from 'mongodb'
 
-export const connect =(mongoUrl:string ='mongodb://localhost:27017')=>
+export const connect =(mongoUrl:string ='mongodb://192.168.0.47:27017')=>
     MongoClient.connect(mongoUrl,{useNewUrlParser:true,useUnifiedTopology:true})
