6ab6ea943b832769e8fae173c0aa4556d7defa2f
HyeonGiMin <hyeoni1995@gmail.com>
2021-02-01 20:53:39 +0900

    20210201

--- 12/ch12-3/src/index.ts
+++ 12/ch12-3/src/index.ts
@@ -1,9 +1,10 @@
-import express from 'express'
-const app=express(), port = 15000
+import {connect} from './mongodb/connect'
+import {runServer} from './runServer'
 
-app.get('/',(req,res)=>res.json({message: 'Hello World!!'}))
-    .get('/users/:skip/:limit',(req,res)=>{
-        const {skip,limit}=req.params
-        res.json({skip,limit})
+connect()
+    .then( async(connection)=>{
+        const db= await connection.db('ch12-2')
+        return db
     })
-    .listen(port ,()=>{console.log(`http://localhost:${port} started...`)})
+    .then(runServer)
+    .catch((e:Error)=>console.log(e.message))
