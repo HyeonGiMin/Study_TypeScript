bff2184be3a404ae49c1534d080db37d651a01a3
HyeonGiMin <hyeoni1995@gmail.com>
2021-02-02 21:07:53 +0900

    210202

---
+++ 12/ch12-4/users/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App: React.FC=()=>{
+  const user = {name:'Jack',age:32}
+  return (
+      <div className= 'App'>{
+          JSON.stringify(user)}
+        </div>
+  )
+}
+
+export default App
